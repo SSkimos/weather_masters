991b1f34527db9180a7e86fc99a1ea8962c9198b
Chereshnev <chereshnev.n.s@edu.mirea.ru>
2022-10-04 01:00:01 +0300

    :wrench: simple database file

---
+++ database/db_operations.py
@@ -0,0 +1,39 @@
+from os import path
+import psycopg2
+from config import username, password, db_name, host, broker
+
+
+class DataBase:
+    def __init__(self):
+        pass
+
+    def connection():
+        connection = psycopg2.connect(
+        database=db_name,
+        password=password,
+        host=broker,
+        port=host
+    )
+
+
+    def insert_object(self, date, temperature, humidity, pressure):
+        conn = DataBase.connection()
+        with conn.cursor() as cursor:
+            cursor.execute(
+                """INSERT INTO data(
+                    mydat,
+                    temperature,
+                    humidity,
+                    pressure
+                ) VALUES (
+                    {date},
+                    {temperature},
+                    {humidity},
+                    {pressure}
+                )
+                """
+            )
+
+            conn.commit()
+            conn.close()
+
